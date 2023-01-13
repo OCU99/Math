969c2c577dcb6d091f61ae07b4e81d2060c44029
Alexander Powell <71793974+OCU99@users.noreply.github.com>
2023-01-13 10:39:55 -0600

    Add files via upload
    I started writing MatrixMath.java in Programming Languages in spring 2021. Or spring of junior year. I recently expanded on it to work with three 3x3 matrices. Interesting.

--- MatrixMath.java
+++ MatrixMath.java
@@ -17,18 +17,51 @@ public class MatrixMath {
                 System.out.print(matrix2[c][d] + "\t");
             }
             System.out.print("\n");
-        } 
+        }
         int [][] matrix3 = new int[3][3];
-        System.out.println("The solution matrix is: ");
+        System.out.println("The third generated matrix is: ");
         for (int e=0; e<3; e++) {
             for (int f=0; f<3; f++) {
-                matrix3[e][f] = 0;
-                for (int g=0; g<3; g++) {
-                    matrix3[e][f] +=matrix1[e][g] * matrix2[g][f];
-                }
+                matrix3[e][f] = (int)(Math.random()*10 + 1);
                 System.out.print(matrix3[e][f] + "\t");
-            } 
+            }
             System.out.print("\n");
         } 
+        int [][] matrix4 = new int[3][3];
+        System.out.println("The fourth matrix is: ");
+        for (int g=0; g<3; g++) {
+            for (int h=0; h<3; h++) {
+                matrix4[g][h] = 0;
+                for (int i=0; i<3; i++) {
+                    matrix4[g][h] += matrix1[g][i] * matrix2[i][h];
+                }
+                System.out.print(matrix4[g][h] + "\t");
+            }
+            System.out.print("\n");
+        }
+        int [][] matrix5 = new int[3][3];
+        System.out.println("The fifth matrix is: ");
+        for (int g=0; g<3; g++) {
+            for (int h=0; h<3; h++) {
+                matrix5[g][h] = 0;
+                for (int i=0; i<3; i++) {
+                    matrix5[g][h] += matrix1[g][i] * matrix3[i][h];
+                }
+                System.out.print(matrix5[g][h] + "\t");
+            }
+            System.out.print("\n");
+        }
+        int [][] matrix6 = new int[3][3];
+        System.out.println("The solution matrix is: ");
+        for (int g=0; g<3; g++) {
+            for (int h=0; h<3; h++) {
+                matrix6[g][h] = 0;
+                for (int i=0; i<3; i++) {
+                    matrix6[g][h] = matrix4[g][h] + matrix5[g][h];
+                }
+                System.out.print(matrix6[g][h] + "\t");
+            }
+            System.out.print("\n");
+        }
     } 
 }
